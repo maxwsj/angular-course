9c32fad82b8e67a7fbb71c2eb7aae0ae5b66e7fd
maxwsj <mw2study@gmail.com>
2026-03-04 18:11:31 -0300

    using the custom component

--- 01-starting-project/src/main.ts
+++ 01-starting-project/src/main.ts
@@ -2,4 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
+// only bootstrapping the root component; other components are brought in via
+// imports and used in its template.
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
