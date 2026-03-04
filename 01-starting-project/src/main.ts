1ef71ee7d0981a877c105f5690850a4e0f0c2c21
maxwsj <mw2study@gmail.com>
2026-03-04 18:29:21 -0300

    styling the header component and adding an image

--- 01-starting-project/src/main.ts
+++ 01-starting-project/src/main.ts
@@ -2,6 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
-// only bootstrapping the root component; other components are brought in via
-// imports and used in its template.
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
