1d6b53a2e283fcb74342332ad673b4d9c4b20c33
maxwsj <mw2study@gmail.com>
2026-03-06 13:22:12 -0300

    defining component inputs to be more flexible

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,17 +7,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  // computed property to generate the image path based on the selected user's avatar using signal and computed
-  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
-
-  // get imagePath() {
-  //   return `assets/users/${this.selectedUser.avatar}`;
-  // }
-
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
   }
+
+  onSelectUser() {}
 }
