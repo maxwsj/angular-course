2b53e515b6380f2cad6b9577a7354c5ba9fba1ef
maxwsj <mw2study@gmail.com>
2026-03-07 21:18:59 -0300

    accepting objects as inputs and adding appropriate typings

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -9,18 +9,20 @@ import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 // Input without signal
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
   @Output() select = new EventEmitter<string>(); // Output decorator with EventEmitter
   // select = output<string>(); // output signal
 
   get imagePath() {
-    return `assets/users/${this.avatar}`;
+    return `assets/users/${this.user.avatar}`;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
 
